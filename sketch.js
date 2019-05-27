dedc3d82ad2ae174ef3b3fc871e205636bca3a15
MrPancakes39 <41960961+MrPancakes39@users.noreply.github.com>
2019-05-27 15:45:33 +0300

    Code! Version 2.0
    added the ability to generate Lagrange Interpolating Polynomial from the coordinates.

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 const xValues = [];
 const yValues = [];
 const blob = [];
-const fileData = "";
+var equation = "";
 
 function setup() {
 	noCanvas();
@@ -9,32 +9,28 @@ function setup() {
 	// Creates a choose file button.
 	createFileInput(fileSelected);
 
-	// Creates a variable "button" from the existing button in html.
-	const button = select("#createTextButton");
-	button.mousePressed(createBlob);
+	// Creates a variable "cButton" from the existing button in html.
+	const cButton = select("#createTextButton");
+	cButton.mousePressed(createBlob);
+
+	const lButton = select("#LIPButton");
+	lButton.mousePressed(generateLIP);
 }
 
 // Checks the file selected if it's a text file and initiates getData();
 function fileSelected(file) {
 	if (file.type == "text" && file.subtype == "plain") {
-		this.fileData = file.data;
-		getData();
+		getData(file.data);
 	} else {
 		createP("I need a text file.");
 	}
 }
 
-// Saves the coordinates as a list in a text file.
-function createBlob() {
-	for (let i = 0; i < xValues.length; i++) {
-		blob.push("x: " + String(xValues[i]) + ", y: " + String(yValues[i]))
-	}
-	saveStrings(blob, 'coodrinateList.txt');
-}
-
 // Gets the data from the file selected and parse it.
-async function getData() {
-	const data = this.fileData;
+async function getData(data) {
+	//const response = await fetch("text/LIP-test.txt");
+	//const data = await response.text();
+	//console.log(data);
 
 	const points = data.split(" ");
 	points.forEach(coordinates => {
@@ -46,4 +42,65 @@ async function getData() {
 		yValues.push(yValue);
 	})
 	createP("Ready");
+	//console.log(xValues);
+	//console.log(yValues);
+}
+
+// Saves the coordinates as a list in a text file.
+function createBlob() {
+	for (let i = 0; i < xValues.length; i++) {
+		blob.push("x: " + String(xValues[i]) + ", y: " + String(yValues[i]))
+	}
+	saveStrings(blob, 'coodrinateList.txt');
+}
+
+function generateLIP() {
+	var i = 0;
+	var j = 0;
+	var k = 0;
+
+	var nom = "";
+	var den = 1;
+
+	const noms = []
+	const dens = [];
+
+	const arrayLength = xValues.length;
+	//console.log(arrayLength);
+
+	while (j < arrayLength) {
+
+		if (k == arrayLength) {
+			k = 0;
+			j++;
+			dens.push(den);
+			den = 1;
+			noms.push(nom + yValues[j - 1]);
+			nom = "";
+		}
+
+		if (j == k) {
+			k++;
+		} else {
+			den *= (float(xValues[j]) - float(xValues[k]));
+			nom += ("(x - " + xValues[k] + ") * ");
+			k++;
+		}
+
+	}
+	//console.log(dens);
+	//console.log(noms);
+
+	while (i < (arrayLength - 1)) {
+		equation += "{" + noms[i] + "} / {" + String(dens[i]) + "}" + " + ";
+		i++;
+	}
+
+	if (i == (arrayLength - 1)) {
+		equation += "{" + noms[i] + "} / {" + String(dens[i]) + "}";
+	}
+
+	console.log(equation);
+
+	saveStrings([equation], 'Curve.txt');
 }
